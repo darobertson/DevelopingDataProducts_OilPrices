a87d283fa088bf02ff0bc26725e2120bf71a79f8
unknown <gundasv@gmail.com>
2015-02-05 23:06:12 +0100

    tried rChart and commented out - too slow

--- server.r
+++ server.r
@@ -2,19 +2,23 @@ library(shiny)
 library(Quandl)
 library(dplyr)
 library(ggplot2)
+library(rCharts)
 
 shinyServer(
     function(input, output) {
         
         data <- Quandl("OPEC/ORB")
         
-        output$latestDate <- renderText ({data[1,"Date"]})
-        output$latestPrice <- renderText ({data[1,"Value"]})
-
         output$oilPlot <- renderPlot({
             plotData <- filter(data, Date > input$dateRange[1], Date < input$dateRange[2] )
             ggplot(plotData, aes(Date, Value)) + geom_line(aes(colour='Actual Price')) + xlab("") + ylab("Price, USD") +
                 geom_smooth(aes(colour='Smoothed Price')) + scale_colour_discrete("")
+        })
+        
+#   rcharts attempt - too slow and bad date formatting        
+#        output$oilPlot <- renderChart({
+#            plotData <- filter(data, Date > input$dateRange[1], Date < input$dateRange[2] )
+#            dPlot(Value ~ Date, data = plotData, type='line')
         })
         
         
